48ceb55953e5a81986b7b93c2d131c7c6e9f26bc
ahmed salah <you@example.com>
2023-04-17 18:00:45 +0200

    Added the full UI

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var tvInput : TextView
+    var lastDot : Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tvInput = findViewById(R.id.tvInput)
+
     }
+    fun setDigit(view:View){
+        tvInput.append((view as Button).text)
+
+    }
+
+    fun onClear(view: View){
+        tvInput.text=""
+        lastDot = true
+    }
+
+    fun onDecimalPoint(view: View){
+        if (lastDot){
+            tvInput.append((view as Button).text)
+            lastDot =  false
+        }
+    }
+
 }
